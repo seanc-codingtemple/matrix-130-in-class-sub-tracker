85bfa2ed201c46c6afd994b67573abd4d45a69a9
seanc-codingtemple <seanc@codingtemple.com>
2023-10-20 17:30:13 -0500

    edit and delete function

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -10,8 +10,11 @@ import {
   doc,
   where,
   deleteDoc,
+  updateDoc,
+  setDoc,
 } from 'firebase/firestore';
-
+import { MdOutlineRemoveCircle } from 'react-icons/md';
+import { GrEdit } from 'react-icons/gr';
 const Account = () => {
   const { user, logout } = UserAuth();
   const [subscriptions, setSubscriptions] = useState([]);
@@ -22,6 +25,8 @@ const Account = () => {
     renewalDate: '',
     notes: '',
   });
+  const [editFormData, setEditFormData] = useState(null);
+  const [editingSubscriptionId, setEditingSubscriptionID] = useState(null);
 
   const navigate = useNavigate();
 
@@ -81,7 +86,45 @@ const Account = () => {
   };
 
   // Update subscription from Firebase
+  const handleEditInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  // start editing function
+  const startEditing = (subscription) => {
+    setEditFormData({
+      service: subscription.service,
+      cost: subscription.cost,
+      startDate: subscription.startDate.toDate().toISOString().split('T')[0],
+      renewalDate: subscription.renewalDate.toDate().toISOString().split('T')[0],
+      notes: subscription.notes,
+    });
+    setEditingSubscriptionID(subscription.id);
+  };
+  // update firebase function
+
+  const updateSubscription = async () => {
+    if (editingSubscriptionId) {
+      const subscriptionRef = doc(db, 'subscriptions', editingSubscriptionId);
+      await updateDoc(subscriptionRef, {
+        service: editFormData.service,
+        cost: parseFloat(editFormData.cost),
+        startDate: new Date(editFormData.startDate),
+        renewalDate: new Date(editFormData.renewalDate),
+        notes: editFormData.notes,
+      });
+      setEditingSubscriptionID(null);
+      setEditFormData(null);
+    }
+  };
+
   // Delete subscription from Firebase
+  const deleteSubscription = async (id) => {
+    await deleteDoc(doc(db, 'subscriptions', id));
+  };
 
   return (
     <div className='max-w-[600px] mx-auto my-16 p-4'>
@@ -143,17 +186,77 @@ const Account = () => {
       </form>
       {subscriptions.map((subscription) => {
         const dateString = subscription.renewalDate.toDate().toDateString();
-
+        const isEditingThisSubscription =
+          editingSubscriptionId === subscription.id;
         return (
-          <li key={subscription.id}className='flex justify-between bg-green-500 p-4 my-2 capitalize' rounded-3xl
->
+          <li
+            key={subscription.id}
+            className='flex justify-between bg-green-500 p-4 my-2 capitalize rounded-3xl'
+          >
             <div className='flex '>
               <p className='ml-2 cursor-pointer'>
                 Subscription: {subscription.service}
               </p>
               <p className='ml-2 cursor-pointer'>$ {subscription.cost}</p>
             </div>
-              <p className='ml-2 cursor-pointer '> Renew Date: {dateString}</p>
+            <p className='ml-2 cursor-pointer '> Renew Date: {dateString}</p>
+            <button onClick={() => deleteSubscription(subscription.id)}>
+              <MdOutlineRemoveCircle size={30} />
+            </button>
+            <button onClick={() => startEditing(subscription)}>
+              <GrEdit size={30} />
+            </button>
+            {isEditingThisSubscription && (
+              <div>
+                <h3>Edit Subscription</h3>
+                <input
+                  value={editFormData.service}
+                  onChange={handleEditInputChange}
+                  className='border p-2 w-full text-xl'
+                  type='text'
+                  placeholder='Enter new service name'
+                  name='service'
+                />
+                <input
+                  value={editFormData.cost}
+                  onChange={handleEditInputChange}
+                  className='border p-2 w-full text-xl'
+                  type='number'
+                  placeholder='Enter subscription cost'
+                  name='cost'
+                />
+                <input
+                  value={editFormData.startDate}
+                  onChange={handleEditInputChange}
+                  className='border p-2 w-full text-xl'
+                  type='date'
+                  placeholder='Enter new service name'
+                  name='startDate'
+                />
+                <input
+                  value={editFormData.renewalDate}
+                  onChange={handleEditInputChange}
+                  className='border p-2 w-full text-xl'
+                  type='date'
+                  placeholder='Enter new service name'
+                  name='renewalDate'
+                />
+                <input
+                  value={editFormData.notes}
+                  onChange={handleEditInputChange}
+                  className='border p-2 w-full text-xl'
+                  type='text'
+                  placeholder='Add some notes...'
+                  name='notes'
+                />
+                <button
+                  className='border p-4 ml-2 mt-2 bg-purple-500 text-slate-100'
+                  onClick={() => updateSubscription(subscription.id)}
+                >
+                  Add New Subscription
+                </button>
+              </div>
+            )}
           </li>
         );
       })}
